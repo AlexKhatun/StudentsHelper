bf84b929db450b240ff1ab3c4d3af4b77fa58e3e
AlexKhatun <khatun3000@gmail.com>
2016-04-29 13:03:19 +0300

    Context+ВиСщттусе

---
+++ DAL/StudentHelperContext.cs
@@ -0,0 +1,23 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class StudentsHelperContext : DbContext
+    {
+        public StudentsHelperContext() : base("StudentsHelper") { }
+
+        public DbSet<Student> Student { get; set; }
+
+        public DbSet<Discipline> Discipline { get; set; }
+
+        public DbSet<Group> Group { get; set; }
+
+        public DbSet<Education> Education { get; set; }
+    }
+}
---
+++ DAL/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace DAL
+{
+    public class Test
+    {
+        public Test()
+        {
+            StudentsHelperContext db = new StudentsHelperContext();
+            db.Group.Add(new Group() { GroupId = 1, Title = "blabla" });
+        }
+    }
+}
